bd60e09cc56910341a9105736965853fed8817ec
manish <manishsingh20250@gmail.com>
2025-09-03 17:44:24 +0530

    configured the render.yml file and updated the requirements.txt and last finishes to portfolio_home.html

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +83,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# If Render provides DATABASE_URL, use Postgres instead
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 
 
 # Password validation
