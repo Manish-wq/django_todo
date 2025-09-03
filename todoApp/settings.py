25108ba6426fe18a9f7608b0bbcea7a0cd68d18d
manish <manishsingh20250@gmail.com>
2025-09-03 18:22:10 +0530

    logs to check db url

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -90,6 +90,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
     DATABASES["default"] = dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 
+print(">>> DATABASE_URL from environment:", os.environ.get("DATABASE_URL"))
+print(">>> DATABASES config:", DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
